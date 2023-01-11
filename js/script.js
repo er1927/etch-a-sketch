bda87b7de048c1e8138ae52eec8f1e9fe7f80187
Elizabeth R <elizabeth.roberts1927@gmail.com>
2023-01-11 10:45:45 -0300

    Added color picker

--- js/script.js
+++ js/script.js
@@ -4,33 +4,34 @@ const colorPicker = document.querySelector("#color");
 const eraseCell = document.querySelector(".fa-eraser");
 const clearAll = document.querySelector(".fa-broom");
 
+let colorPicked =
+
 createGrid = () => {
     for (let i = 0; i < 256; i++) {
         const div = document.createElement('div');
         div.classList.add('grid-element');
         div.addEventListener('mouseover', function(event){
-            event.target.style.backgroundColor = "pink";
+            event.target.style.backgroundColor = colorPicked;
         })
         grid.appendChild(div); 
     }
     };
     createGrid();
 
-    colorPicker.addEventListener("click", showsColors);
+    colorPicker.addEventListener("input", showsColors);
     eraseCell.addEventListener("click", erasesByCell);
     clearAll.addEventListener("click", clearsGrid);
 
-function showsColors() {
-    // console.log("You've clicked on the color picker") // works
+function showsColors(e) {
+    // /console.log("You've clicked on the color picker") // works
+            colorPicked = e.target.value; //IT WORKS!   
+    }
 
-}
 
 function erasesByCell() {
     console.log("You've clicked on the eraser") // works   
     
 }
-
-
 
 function clearsGrid() {
     // console.log("You've clicked on the clear grid lil broom") // works
